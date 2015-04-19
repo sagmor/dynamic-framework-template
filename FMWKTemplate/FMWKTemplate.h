d8e03ae1fb1fbf8321f6d095e708162873b26af3
Seba Gamboa <me@sagmor.com>
2015-04-19 14:05:58 -0300

    Initial Commit

---
+++ FMWKTemplate/FMWKTemplate.h
@@ -0,0 +1,19 @@
+//
+//  FMWKTemplate.h
+//  FMWKTemplate
+//
+//  Created by Seba Gamboa on 4/19/15.
+//  Copyright (c) 2015 SagMor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FMWKTemplate.
+FOUNDATION_EXPORT double FMWKTemplateVersionNumber;
+
+//! Project version string for FMWKTemplate.
+FOUNDATION_EXPORT const unsigned char FMWKTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMWKTemplate/PublicHeader.h>
+
+
